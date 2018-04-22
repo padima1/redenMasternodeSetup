abe815bd060ec5aebe8eb25046983e5971e6e034
fasterpool <37882666+fasterpool@users.noreply.github.com>
2018-04-21 17:04:17 -0700

    Update reden-setup.sh

--- reden-setup.sh
+++ reden-setup.sh
@@ -11,7 +11,9 @@ sleep 5
 
 # update packages and upgrade Ubuntu
 cd ~
-sudo apt-get update -y && apt-get upgrade -y && apt-get autoremove -y
+sudo apt-get -y update
+sudo apt-get -y upgrade
+sudo apt-get -y autoremove
 sudo apt-get -y install wget nano htop git
 sudo apt-get -y install libzmq3-dev
 sudo apt-get -y install libboost-system-dev libboost-filesystem-dev libboost-chrono-dev libboost-program-options-dev libboost-test-dev libboost-thread-dev
