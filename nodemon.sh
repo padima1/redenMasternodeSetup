57da01b998f137aeea58d882c858ae890d283820
fasterpool <37882666+fasterpool@users.noreply.github.com>
2018-04-26 15:02:15 -0700

    Added parameters for refresh time and datadir index

--- nodemon.sh
+++ nodemon.sh
@@ -1,21 +1,33 @@
-#It is a one-liner script for now. Refresh time is 1 sec.
+#!/bin/bash
+# nodemon 1.0 - Reden Masternode Monitoring 
+
+#Processing command line params
+if [ -z $1 ]; then dly=1; else dly=$1; fi   # Default refresh time is 1 sec
+
+datadir="/$USER/.redencore$2"   # Default datadir is /root/.redencore
+ 
+# Install jq if it's not present
 dpkg -s jq 2>/dev/null >/dev/null || sudo apt-get -y install jq
 
-watch -ptn 10 "echo '===========================================================================
-Outbound connections to other nodes (source: getpeerinfo, inbound excluded)
+#It is a one-liner script for now
+watch -ptn $dly "echo '===========================================================================
+Outbound connections to other Reden nodes [reden datadir: $datadir]
 ===========================================================================
-Node IP                Ping   Rx/Tx     Since  Hdrs   Height  Time   Ban
-Address                (ms)  (KBytes)   Block  Syncd  Blocks  (min)  Score
+Node IP                Ping    Rx/Tx     Since  Hdrs   Height  Time   Ban
+Address                (ms)   (KBytes)   Block  Syncd  Blocks  (min)  Score
 ==========================================================================='
-reden-cli getpeerinfo | jq -r '.[] | select(.inbound==false) | \"\(.addr),\(.pingtime*1000|floor),\
+reden-cli -datadir=$datadir getpeerinfo | jq -r '.[] | select(.inbound==false) | \"\(.addr),\(.pingtime*1000|floor),\
 \(.bytesrecv/1024|floor)/\(.bytessent/1024|floor),\(.startingheight) ,\(.synced_headers) ,\(.synced_blocks)  ,\
 \((now-.conntime)/60|floor) ,\(.banscore)\"' | column -t -s ',' && 
 echo '==========================================================================='
-echo 'Your Masternode Status: \n# reden-cli masternode status' && reden-cli masternode status &&
+uptime
 echo '==========================================================================='
-echo 'Sync Status: \n# reden-cli mnsync status' &&  reden-cli mnsync status &&
+echo 'Masternode Status: \n# reden-cli -datadir=$datadir masternode status' && reden-cli masternode status
 echo '==========================================================================='
-echo 'Masternode Information: \n# reden-cli getinfo' && reden-cli getinfo &&
+echo 'Sync Status: \n# reden-cli -datadir=$datadir mnsync status' &&  reden-cli mnsync status
 echo '==========================================================================='
+echo 'Masternode Information: \n# reden-cli -datadir=$datadir getinfo' && reden-cli getinfo
+echo '==========================================================================='
+echo 'Usage: nodemon.sh [refresh delay] [datadir index]'
+echo 'Example: nodemon.sh 10 22 will run every 10 seconds and query redend in /$USER/.redencore22'
 echo '\n\nPress Ctrl-C to Exit...'"
-
