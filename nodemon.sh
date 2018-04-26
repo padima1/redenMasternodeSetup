53a91f25314da815f0c94fdfb5ff23c896319ed1
fasterpool <37882666+fasterpool@users.noreply.github.com>
2018-04-25 21:53:34 -0700

    improving nodemon accuracy

--- nodemon.sh
+++ nodemon.sh
@@ -3,10 +3,11 @@ dpkg -s jq 2>/dev/null >/dev/null || sudo apt-get -y install jq
 
 watch -ptn 1 "echo '=========================================================================================================
 Outbound connections to other peer nodes (from reden-cli getpeerinfo, excluding inbound connections)
-1:ID 2:NodeIP 3:Ping,ms 4:Sent,KB 5:Recvd,KB 6:StartBlk 7:HdrsSyncd 8:BlkSyncd 9:ConnTime,min 10:BanScore
+ID     Node IP                                        Ping  Rx/Tx    Since Hdrs  Height  Conn  Ban
+       Address                                        (ms) (KBytes)  Block Syncd Blocks  (min) Score
 ========================================================================================================='
 reden-cli getpeerinfo | jq -r '.[] | select(.inbound==false) | \"\(.id),\(.addr),\(.pingtime*1000|floor),\
-\(.bytessent/1000|floor),\(.bytesrecv/1000|floor),\(.startingheight),\(.synced_headers),\(.synced_blocks),\
+\(.bytesrecv/1024|floor)/\(.bytessent/1024|floor),\(.startingheight),\(.synced_headers),\(.synced_blocks),\
 \((now-.conntime)/60|floor),\(.banscore)\"' | column -t -s ',' && 
 echo '========================================================================================================='
 echo 'Your Masternode Status: \n# reden-cli masternode status' && reden-cli masternode status &&
